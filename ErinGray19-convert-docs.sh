f4394e3daa02f1b9fcc42aff90d70f313a3a69f8
Erin Gallagher <ep74468@gmail.com>
2016-10-24 20:34:54 +0000

    renamed cloned respository, and uploaded a dummy file to convert

--- ErinGray19-convert-docs.sh
+++ ErinGray19-convert-docs.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
 
 
 # Declare input argument as a variable
-
+title="Assignment 3
+"
 # Convert markdown to HTML
+pandoc -o example.html example.md
 
 # Convert markdown to DOCX
+pandoc -o example.docx example.md
 
 # Convert markdown to ODT
+pandoc -o example.odt example.md
 
 # Convert markdown to PDF
+pandoc -o example.pdf example.md
+
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF"
