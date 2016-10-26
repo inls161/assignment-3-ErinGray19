ddd6c64f79f6f9d8f7e984d86184b0271bce7bf2
Erin Gallagher <ep74468@gmail.com>
2016-10-26 19:53:19 +0000

    replaced file to official paper with added pictures

--- ErinGray19-convert-docs.sh
+++ ErinGray19-convert-docs.sh
@@ -5,20 +5,20 @@
 title="Assignment 3
 "
 # Convert markdown to HTML
-pandoc -o NotesonRainPaper.html NotesonRainPaper.md
+pandoc -o WrightBrothersPatent.html WrightBrothersPatent.md
 
 # Convert markdown to DOCX
-pandoc -o NotesonRainPaper.docx NotesonRainPaper.md
+pandoc -o WrightBrothersPatent.docx WrightBrothersPatent.md
 
 # Convert markdown to ODT
-pandoc -o NotesonRainPaper.odt NotesonRainPaper.md
+pandoc -o WrightBrothersPatent.odt WrightBrothersPatent.md
 
 # Convert markdown to PDF
-pandoc -o NotesonRainPaper.pdf NotesonRainPaper.md
+pandoc -o WrightBrothersPatent.pdf WrightBrothersPatent.md
 
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
-echo "Converted NotesonRainPaper to HTML, DOCX, ODT, PDF"
+echo "Converted WrightBrothersPatent to HTML, DOCX, ODT, PDF"
 
  today() {
     echo -n "Files Created: "
